ab6842fec432d14f33c91d85ac8dd28cd6764ccb
chensong <15850774503@163.com>
2020-02-15 00:59:13 +0800

    [git 合并分支的命令 merge 记录]

--- Tools/git.sh
+++ Tools/git.sh
@@ -10,3 +10,20 @@ git push origin --delete develop
 
 #删除本地分支
 git branch -d develop 
+
+
+#一、开发分支（chensong_develop）上的代码达到上线的标准后，要合并到 master 分支
+
+git checkout chensong_develop
+git pull
+git checkout master
+git merge chensong_develop
+git push -u origin master
+
+#二、当master代码改动了，需要更新开发分支（chensong_develop）上的代码
+
+git checkout master 
+git pull 
+git checkout chensong_develop
+git merge master 
+git push -u origin chensong_develop
